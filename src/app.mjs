330c31aee6fb2451cfcfd56e5075e64dcbda2691
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-06 17:01:21 +0800

    feat(app): setup app
    Setup mongodb connection
    Import dirname and use to get views and public dir
    Import the main route and chain it in the
        middleware
    Import the passport config file and chain it in the
        middleware
    Import the session config file and chain it in the
        middleware

--- src/app.mjs
+++ src/app.mjs
@@ -1,29 +1,50 @@
 import 'dotenv/config';
 import express from "express";
+import mongoose from 'mongoose';
+import passport from 'passport';
+import __dirname from '../dirname.mjs';
 import createError from "http-errors";
 import { join } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import indexRouter from "./routes/index.mjs";
-import usersRouter from "./routes/users.mjs";
-
-// Init dir name variable
-const { dirname } = import.meta;
+import * as PassportConfig from './configs/passport.mjs';
+import SessionConfig from './configs/session.mjs';
+import mainRouter from './routes/main.mjs';
 
 const app = express();
 
+// Define the database URL to connect to.
+const mongoDb = process.env.MONGO_DB;
+
+// Wait for database to connect, logging an error if there is a problem
+const main = async () => await mongoose.connect(mongoDb);
+main().catch(console.error);
+
+// Passport local strategy
+passport.use(PassportConfig.strategy);
+
+passport.serializeUser(PassportConfig.serializeUser);
+passport.deserializeUser(PassportConfig.deserializeUser);
+
 // view engine setup
-app.set("views", join(dirname, "views"));
+app.set("views", join(__dirname, "src/views"));
 app.set("view engine", "pug");
 
+app.use(SessionConfig);
+app.use(passport.session());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(dirname, "public")));
+app.use(express.static(join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.locals.currentUser = req?.user;
+  res.locals.isMember = req.user?.member;
+  res.locals.isAdmin = req.user?.admin;
+  next()
+});
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", mainRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
