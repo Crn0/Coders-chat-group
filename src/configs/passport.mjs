c3b449e58929082c7c184e714ba6a1ca5e716828
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-08 18:37:11 +0800

    fix(passport): fix password bug and remove import

--- src/configs/passport.mjs
+++ src/configs/passport.mjs
@@ -1,15 +1,16 @@
-import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import bcrypt from "bcryptjs";
 import User from "../models/user.mjs";
 
-const verifyCb = async (password, username, done) => {
+const verifyCb = async (username, password, done) => {
   try {
     const user = await User.findOne({ username }).exec();
+    
+    if (!user) return done(null, false);
+
     const match = await bcrypt.compare(password, user.password);
 
-    if (!user) return done(null, false, { message: "Incorrect username" });
-    if (!match) return done(null, false, { message: "Incorrect password" });
+    if (!match) return done(null, false);
 
     return done(null, user);
   } catch (error) {
