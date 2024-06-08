eb928bc51cc9094e1def2a744136dab2b7a9a377
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-08 18:38:30 +0800

    feat(controllers): define controllers
    Define logic of index, register and login GET
        request
    Validate registration POST request
    Authenticate user login

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,32 +1,59 @@
+import 'dotenv/config';
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from 'express-validator';
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import * as Authenticate from '../middlewares/authMiddleware.mjs';
+import User from '../models/user.mjs';
+import usernameExist from "../helpers/usernameExist.mjs";
+import isPasswordMatch from '../helpers/passwordMatch.mjs';
 
 // GET
-const index = asyncHandler(async (req, res, _) => {
-  res.send("Index page: GET NOT IMPLEMENTED");
-});
+const index = [
+  Authenticate.isAuth(false),
+  (req, res, _) => {
+    if(req.isAuthenticated()) return res.redirect('/minor_arcana');
+  
+    res.render('index', {
+      title: 'Tarot Club'
+    });
+  }
+];
 
 const home = asyncHandler(async (req, res, _) => {
-  res.send("Home page: GET NOT IMPLEMENTED");
+  res.send("<a href='/logout'>Logout<a/>");
 });
 
 const secret_page = asyncHandler(async (req, res, _) => {
-  res.send("Secret page: GET NOT IMPLEMENTED");
+  res.send("<a href='/logout'>Logout<a/>");
 });
 
 const profile = asyncHandler(async (req, res, _) => {
   res.send("Profile page: GET NOT IMPLEMENTED");
 });
 
-const register_get = asyncHandler(async (req, res, _) => {
-  res.send("Register page: GET NOT IMPLEMENTED");
-});
+const register_get = [
+  Authenticate.ifAuth((req, res, _) => res.redirect('/minor_arcana')),
+  (req, res, _) => {
+
+    res.render('register_form', {
+      title: 'Register'
+    });
+  }
+];
 
 const login_get = asyncHandler(async (req, res, _) => {
-  res.send("Login page: GET NOT IMPLEMENTED");
+  res.render('login_form', {
+    title: 'login'
+  });
 });
 
-const logout_get = asyncHandler(async (req, res, _) => {
-  res.send("Logout page: GET NOT IMPLEMENTED");
+const logout_get = asyncHandler(async (req, res, next) => {
+  req.session.destroy(err => {
+    if(err) return next(err);
+
+    res.redirect('/')
+  });
 });
 
 const delete_get = asyncHandler(async (req, res, _) => {
@@ -34,12 +61,73 @@ const delete_get = asyncHandler(async (req, res, _) => {
 });
 
 // POST
-const register_post = asyncHandler(async (req, res, _) => {
-  res.send("Register page: POST NOT IMPLEMENTED");
-});
+const register_post = [
+  body('username')
+  .trim()
+  .isLength({ min: 3 })
+  .withMessage('Username must be minimum of 4 characters')
+  .custom(usernameExist)
+  .escape(),
+  body('password')
+  .trim()
+  .escape(),
+  body('password_confirm')
+  .trim()
+  .custom(isPasswordMatch)
+  .withMessage('Passwords do not match')
+  .escape(),
+  body('admin_password')
+  .trim()
+  .escape(),
+  body('member_password')
+  .trim()
+  .escape(),
+  asyncHandler( async (req, res, next) => {
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()) {
+      return res.render('register_form', {
+        title: 'Register',
+        uname: req.body.username,
+        errors: errors.array()
+      });
+    }
 
-const login_post = asyncHandler(async (req, res, _) => {
-  res.send("Login page: POST NOT IMPLEMENTED");
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      try {
+        const user = new User({
+          username: req.body.username,
+          password: hashedPassword,
+        });
+
+        if(err) throw new Error(err);
+
+        if(req.body.admin_password === process.env.ADMIN_PASSWORD) {
+          user.admin = true;
+        }
+        if(req.body.member_password === process.env.MEMBER_PASSWORD) {
+          user.member = true;
+        }
+
+        await user.save();
+
+        req.login(user, (err) => {
+          if(err) return next(err);
+          if(user.member || user.admin) return res.redirect('/major_arcana')
+
+          res.redirect('/minor_arcana')
+        })
+      } catch (error) {
+        return next(error);
+      }
+    })
+  })
+];
+
+const login_post = passport.authenticate('local', {
+  successRedirect: '/minor_arcana',
+  failureRedirect: '/login',
+  failureMessage: 'Incorrect username or password',
 });
 
 const delete_post = asyncHandler(async (req, res, _) => {
