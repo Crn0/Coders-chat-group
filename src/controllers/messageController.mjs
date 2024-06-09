af1123ee0e83bc4b3470a947c3782bf1467ae4e8
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-09 19:42:00 +0800

    feat: implement post and get request

--- src/controllers/messageController.mjs
+++ src/controllers/messageController.mjs
@@ -1,14 +1,43 @@
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from 'express-validator';
+import Message from '../models/message.mjs';
+import * as Authenticate from '../middlewares/authMiddleware.mjs';
+
 
 // GET
-const new_get = asyncHandler(async (req, res, _) => {
-  res.send("Message page: GET NOT IMPLEMENTED");
-});
+const new_get = [
+  Authenticate.isAuthProtectedRoute(true),
+  asyncHandler(async (req, res, _) => {
+    res.render('message_form', {
+      title: 'New Message'
+    });
+  })
+];
 
 // POST
-const new_post = asyncHandler(async (req, res, _) => {
-  res.send("Message page: POST NOT IMPLEMENTED");
-});
+const new_post = [
+  body('title')
+  .trim()
+  .escape(),
+  body('message')
+  .trim()
+  .escape(),
+
+  asyncHandler( async (req, res, _) => {
+    const id = res.locals.currentUser._id;
+    const { title, message } = req.body;
+
+    const newMessage = new Message({
+      title,
+      message,
+      author: id
+    });
+    
+    await newMessage.save();
+
+    res.redirect('/');
+  })
+];
 
 const delete_post = asyncHandler(async (req, res, _) => {
   res.send("Message delete: POST NOT IMPLEMENTED");
