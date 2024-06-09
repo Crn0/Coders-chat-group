5082af3adf6c89daf9a43f2e09cc66d1ec3aca93
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-09 19:30:27 +0800

    refactor(models): change fields
    Remove messages fields in user model
    Add author field in message model
    Add secret field in message model to mark
        member messages

--- src/models/message.mjs
+++ src/models/message.mjs
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
   title: { type: String, minLength: 1, maxLength: 100, required: true },
   message: { type: String, minLength: 1, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  secret: { type: Boolean, default: false },
   date: { type: Date, default: Date },
 });
 
--- src/models/user.mjs
+++ src/models/user.mjs
@@ -7,8 +7,16 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
-  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
-  secretMessages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  // messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  // secretMessages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 });
+
+userSchema.virtual('profile').get(function() {
+  return `/user/${this._id}`
+})
+
+userSchema.virtual('delete').get(function() {
+  return `/user/${this._id}/delete`
+})
 
 export default mongoose.model("User", userSchema);
