f50d4e210af53f6b5a3f150e5af17d0bc6a9b146
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-10 17:42:22 +0800

    feat(models/virtual): format date

--- src/models/message.mjs
+++ src/models/message.mjs
@@ -10,4 +10,15 @@ const messageSchema = new Schema({
   date: { type: Date, default: Date },
 });
 
+messageSchema.virtual('dateFormatted').get(function() {
+  const options = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  return this.date.toLocaleDateString([], options).split(' ').slice(1).join(' ');
+})
+
 export default mongoose.model("Message", messageSchema);
