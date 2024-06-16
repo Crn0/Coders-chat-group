c893663dcdf6a2a704e0ee67b57460e041b3c46e
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-16 15:34:28 +0800

    refactor(model/user): remove min and max length for username

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: { type: String, minLength: 3, maxLength: 100, required: true },
+    username: { type: String,  required: true },
     password: { type: String, required: true },
     member: { type: Boolean, default: false },
     admin: { type: Boolean, default: false },
