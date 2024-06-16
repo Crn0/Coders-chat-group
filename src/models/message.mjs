949755423c6a5bb26c0ebad63db577bf48475aa0
Crno0 <123456789+odin@users.noreply.github.com>
2024-06-16 15:35:13 +0800

    refactor(model/message): remove min and max length for title and post

--- src/models/message.mjs
+++ src/models/message.mjs
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    title: { type: String, minLength: 1, maxLength: 100, required: true },
-    message: { type: String, minLength: 1, required: true },
+    title: { type: String, required: true },
+    message: { type: String, required: true },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
     secret: { type: Boolean, default: false },
     date: { type: Date, default: Date },
